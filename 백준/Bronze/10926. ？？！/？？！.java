eeb545665aa203c5a4e8ede56ce95ecee6104ce1
LeeMinGyeong007 <1313rhtrht@gmail.com>
2026-01-12 17:31:00 +0900

    [Bronze V] Title: ??!, Time: 160 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String arg[]) {
+	Scanner sc= new Scanner(System.in);
+	String str1 = sc.next();
+	System.out.println(str1+"??!");
+	
+	sc.close();
+	}
+}
