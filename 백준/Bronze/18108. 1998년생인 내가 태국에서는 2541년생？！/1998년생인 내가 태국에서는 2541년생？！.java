9c5117bd4b38eb4bc8723d624b1ea3b15b161f2a
LeeMinGyeong007 <1313rhtrht@gmail.com>
2026-01-12 17:26:22 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int x;
+		int y=sc.nextInt();
+		x=y-543;
+		System.out.println(x);
+		sc.close();
+	}
+}
